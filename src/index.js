dbecbb1c806a5bb2540a63e95be36d6e258f124f
Nechai_Ihor <fredduke1983@gmail.com>
2023-02-12 17:49:35 +0200

    up

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ const KEY = '33584211-0b8ad53b88131ae018d3e6558';
 axios
   .get(`https://pixabay.com/api/?key=${KEY}&q=yellow+flowers&image_type=photo`)
   .then(res => {
-    console.log(res.data);
+    res.data.hits.forEach(element => {
+      console.log(element);
+    });
   });
