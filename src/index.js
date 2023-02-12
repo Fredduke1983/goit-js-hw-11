49e15c639523710419a827f405113ebe2570d8b4
Nechai_Ihor <fredduke1983@gmail.com>
2023-02-12 21:59:36 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import axios from 'axios';
 
 const KEY = '33584211-0b8ad53b88131ae018d3e6558';
+const URL = 'https://pixabay.com/api/';
+const searchForm = document.querySelector('.search-form');
+const loadMore = document.querySelector('.load_more');
+let counterPage = 1;
+let inputValue = null;
 
-axios
-  .get(`https://pixabay.com/api/?key=${KEY}&q=yellow+flowers&image_type=photo`)
-  .then(res => {
-    res.data.hits.forEach(element => {
-      console.log(element);
+searchForm.addEventListener('input', onSearchInput);
+searchForm.addEventListener('submit', onSubmitForm);
+loadMore.addEventListener('click', onBtnMore);
+
+function onSearchInput(e) {
+  inputValue = e.target.value;
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  counterPage = 1;
+  e.target.reset();
+  getPixa();
+}
+
+function onBtnMore() {
+  counterPage += 1;
+  getPixa();
+}
+
+function getPixa() {
+  axios
+    .get(
+      `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${counterPage}`
+    )
+    .then(res => {
+      if (res.data.hits.length !== 0) {
+        res.data.hits.forEach(element => {
+          console.log(element.tags);
+        });
+      } else console.log('not imgs');
     });
-  });
+}
