347b3112964cfd9492b162d0afa4392f6094ede1
Fredduke1983 <fredduke1983@gmail.com>
2023-02-14 10:20:22 +0200

    up

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ const KEY = '33584211-0b8ad53b88131ae018d3e6558';
 const URL = 'https://pixabay.com/api/';
 const searchForm = document.querySelector('.search-form');
 const loadMore = document.querySelector('.load_more');
+const gallery = document.querySelector('.gallery');
+
 let counterPage = 1;
 let inputValue = null;
+let galleryRef = null;
 
 searchForm.addEventListener('input', onSearchInput);
 searchForm.addEventListener('submit', onSubmitForm);
@@ -31,7 +34,27 @@ async function getPixa() {
   const response = await axios.get(
     `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${counterPage}`
   );
-  console.log(response.data);
+  galleryRef = await response.data.hits.map(element => {
+    return `<div class="photo-card">
+                          <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+                          <div class="info">
+                            <p class="info-item">
+                              <b>Likes: ${element.likes}</b>
+                            </p>
+                            <p class="info-item">
+                              <b>Views: ${element.views}</b>
+                            </p>
+                            <p class="info-item">
+                              <b>Comments: ${element.comments}</b>
+                            </p>
+                            <p class="info-item">
+                              <b>Downloads: ${element.downloads}</b>
+                            </p>
+                          </div>
+                        </div>`;
+  });
+  gallery.insertAdjacentHTML('beforeend', galleryRef.join());
+
   // axios
   //   .get(
   //     `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${counterPage}`
