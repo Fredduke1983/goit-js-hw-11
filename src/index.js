79bb95199b182f49396922efa82c168b29ac8693
Fredduke1983 <fredduke1983@gmail.com>
2023-02-15 11:40:54 +0200

    up

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ async function getPixa() {
                           </div>
                         </div>`;
   });
-  console.log(response.data.hits, response.data.hits.totalHits);
+  console.log(response.data.hits, response.data.totalHits);
   if (inputValue.length !== 0 && galleryRef.length === 0) {
     console.log('no images');
   } else {
